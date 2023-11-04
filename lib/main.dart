0ba0236c7049bbb732d35085fe0fa3897ff5ebde
CHOI HYUK <8167chhk@naver.com>
2023-11-04 21:54:37 +0900

    테마색 설정함

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test1/interface.dart';
+import 'package:test1/login_widgets/login_ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,18 +16,20 @@ class MyApp extends StatelessWidget {
       //앱 테마
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+          backgroundColor: const Color.fromARGB(255, 206, 250, 226),
         ),
         textTheme: const TextTheme(
           displayLarge: TextStyle(
             color: Color(0xFF232B55),
           ),
         ),
-        primaryColor: const Color.fromARGB(255, 246, 243, 243),
-        cardColor: const Color.fromARGB(255, 224, 224, 224),
+        primaryColorLight: const Color.fromARGB(255, 168, 255, 208),
+        primaryColor: const Color.fromARGB(255, 75, 255, 195),
+        primaryColorDark: const Color.fromARGB(124, 73, 236, 195),
+        cardColor: const Color.fromARGB(255, 233, 255, 243),
       ),
       //InterFace 호출
-      home: const InterFace(),
+      home: const LoginUI(),
     );
   }
 }
