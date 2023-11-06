2386c55b26e922878e32ecc97ea885ebcac3598b
kwondoha <ehehgk@naver.com>
2023-11-06 11:35:28 +0900

    DB 연동, 로그인 기능 구현

---
+++ android/bmt/android/app/src/main/kotlin/com/example/bmt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bmt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:test1/login_widgets/login_ui.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); //db 연동
   runApp(const MyApp());
 }
 
