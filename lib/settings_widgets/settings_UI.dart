0163dae671e3bce6b4d58b5b12d785f139232519
CHOI HYUK <8167chhk@naver.com>
2023-11-07 20:14:48 +0900

    설정 페이지

--- lib/settings_widgets/settings_UI.dart
+++ lib/settings_widgets/settings_UI.dart
@@ -14,115 +14,234 @@ class _SettingsUIState extends State<SettingsUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            color: Colors.black,
-          ), // 뒤로 가기 아이콘
-          onPressed: () {
-            Navigator.pop(context); // 뒤로 가기 버튼을 누르면 현재 화면에서 빠져나감
-          },
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              color: Colors.black,
+            ), // 뒤로 가기 아이콘
+            onPressed: () {
+              Navigator.pop(context); // 뒤로 가기 버튼을 누르면 현재 화면에서 빠져나감
+            },
+          ),
+          title: const Text(
+            '설정',
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+          centerTitle: true,
         ),
-        title: const Text(
-          '설정',
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Theme.of(context).primaryColorLight,
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SettingAccount(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "서비스 설정",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Theme.of(context).cardColor, // 텍스트와 아이콘의 색상
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 16, vertical: 8), // 패딩 설정
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10.0), // 버튼의 모서리를 둥글게 만듭니다.
                 ),
               ),
-              child: const Text(
-                '계정 설정',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100),
-              ),
-            ),
-            const SizedBox(height: 6.5),
-            ElevatedButton(
-              onPressed: () {
-                // 여기에 도움말을 위한 페이지로 이동하는 코드 추가
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Theme.of(context).cardColor, // 텍스트와 아이콘의 색상
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 16, vertical: 8), // 패딩 설정
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10.0), // 버튼의 모서리를 둥글게 만듭니다.
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                    color: Colors.grey, // 테두리 색상
+                    width: 0.5, // 테두리 두께
+                  ),
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "위젯 설정",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
                 ),
               ),
-              child: const Text(
-                '도움말',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100),
-              ),
-            ),
-            const SizedBox(height: 6.5),
-            ElevatedButton(
-              onPressed: () {
-                // 여기에 공지 사항을 위한 페이지로 이동하는 코드 추가
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Theme.of(context).cardColor, // 텍스트와 아이콘의 색상
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 16, vertical: 8), // 패딩 설정
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10.0), // 버튼의 모서리를 둥글게 만듭니다.
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "계정 설정",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              child: const Text(
-                '공지 사항',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100),
-              ),
-            ),
-            const SizedBox(height: 6.5),
-            ElevatedButton(
-              onPressed: () {
-                // 여기에 앱 정보를 위한 페이지로 이동하는 코드 추가
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Theme.of(context).cardColor, // 텍스트와 아이콘의 색상
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 16, vertical: 8), // 패딩 설정
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(10.0), // 버튼의 모서리를 둥글게 만듭니다.
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                    color: Colors.grey, // 테두리 색상
+                    width: 0.5, // 테두리 두께
+                  ),
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "계정 정보",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
                 ),
               ),
-              child: const Text(
-                '앱 정보',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                    color: Colors.grey, // 테두리 색상
+                    width: 0.5, // 테두리 두께
+                  ),
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "위치정보 이용 동의",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                    color: Colors.grey, // 테두리 색상
+                    width: 0.5, // 테두리 두께
+                  ),
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "즐겨찾기 백업",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "도움말",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                    color: Colors.grey, // 테두리 색상
+                    width: 0.5, // 테두리 두께
+                  ),
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "문의하기",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                  border: Border.all(
+                    color: Colors.grey, // 테두리 색상
+                    width: 0.5, // 테두리 두께
+                  ),
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "약간 및 정책",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                ),
+                width: double.infinity,
+                height: 50,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 8.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "위젯 설정",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
