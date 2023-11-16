e835ae9a02126e65d9dfd7ed98e1319b48c1471f
CHOI HYUK <8167chhk@naver.com>
2023-11-17 03:58:54 +0900

    경로 결과 보여주는 코드인데 아직 구현안함

---
+++ lib/search_widgets/route_result_UI.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class RouteResults extends StatefulWidget {
+  const RouteResults({super.key});
+
+  @override
+  State<RouteResults> createState() => _RouteResultsState();
+}
+
+class _RouteResultsState extends State<RouteResults> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          width: 90,
+          height: 35,
+          child: const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "앱 추천 경로",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          width: 90,
+          height: 35,
+          child: const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "최단 시간",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          width: 90,
+          height: 35,
+          child: const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "최소 비용",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+}
