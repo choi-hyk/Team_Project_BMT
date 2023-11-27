0d3c7faaf8ecd0593085eacf01eacb0e3ed72d63
kwondoha <ehehgk@naver.com>
2023-11-27 10:45:34 +0900

    약관 내용과 약관 ui 분리

--- lib/settings_widgets/location_terms.dart
+++ lib/settings_widgets/location_terms.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<String> loadTerms() async {
+  return await rootBundle.loadString('assets/terms/location_terms.txt');
+}
 
 class Location_Terms extends StatefulWidget {
   const Location_Terms({super.key});
@@ -12,29 +17,25 @@ class _Location_TermsState extends State<Location_Terms> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('위치정보 이용약관'),
+        title: Text("위치정보 이용약관"),
       ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          "제 1 조 (목적)\n"
-          "본 약관은 BMT가 제공하는 사물위치정보 및 위치기반 서비스(이하, 위치정보 서비스)에 대해 회사와 서비스를 이용하는 이용자간의 권리·의무 및 책임사항, 기타 필요한 사항 규정을 목적으로 합니다.\n"
-          "제 2 조 (이용약관의 효력 및 변경)\n"
-          "①본 약관은 이용자가 본 약관에 동의하고 회사가 정한 절차에 따라 위치정보 서비스의 이용자로 등록됨으로써 효력이 발생합니다.\n"
-          "②이용자가 본 약관의 “동의하기” 버튼을 클릭하였을 경우 본 약관의 내용을 모두 읽고 이를 충분히 이해하였으며, 그 적용에 동의한 것으로 봅니다.\n"
-          "③회사는 위치정보 서비스의 변경사항을 반영하기 위한 목적 등으로 필요한 경우 관련 법령을 위배하지 않는 범위에서 본 약관을 수정할 수 있습니다.\n"
-          "④약관이 변경되는 경우 회사는 변경사항을 그 적용일자 최소 15일 전에 서비스 공지사항을 통해 공지합니다.\n"
-          "⑤회사가 전항에 따라 공지 또는 통지를 하면서 공지 또는 통지일로부터 개정약관 시행일 7일 후까지 거부의사를 표시하지 아니하면 승인한 것으로 본다는 뜻을 명확하게 고지하였음에도 이용자의 의사표시가 없는 경우에는 변경된 약관을 승인한 것으로 봅니다. 이용자가 개정약관에 동의하지 않을 경우 본 약관에 대한 동의를 철회할 수 있습니다.\n"
-          "제 3 조 (약관 외 준칙)\n"
-          "이 약관에 명시되지 않은 사항에 대해서는 위치 정보의 보호 및 이용 등에 관한 법률, 개인정보보호법, 정보보호 등에 관한 법률 등 관계법령 및 회사가 정한 지침 등의 규정에 따릅니다.\n"
-          "제 4 조 (서비스의 내용)\n"
-          "BMT는 위치정보사업자로부터 수집한 이용자의 위치정보를 이용하여 아래와 같은 위치정보 서비스를 제공합니다.\n"
-          "①지하철 사용을 위한 위치 공유, 위치/지역에 따른 알림, 경로 안내\n"
-          "제 5 조 (서비스 이용요금)\n"
-          "회사가 제공하는 위치정보 서비스는 무료입니다.\n"
-          "단, 무선 서비스 이용 시 발생하는 데이터 통신료는 별도이며, 이용자가 가입한 각 이동통신사의 정책에 따릅니다.\n",
-          style: TextStyle(fontSize: 16.0),
-        ),
+      body: FutureBuilder<String>(
+        future: loadTerms(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // 데이터가 로드되면 표시
+            return SingleChildScrollView(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                snapshot.data ?? '내용을 불러오는 데 실패했습니다.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            );
+          } else {
+            // 데이터 로딩 중에는 로딩 인디케이터 표시
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
--- lib/settings_widgets/privacy_terms.dart
+++ lib/settings_widgets/privacy_terms.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<String> loadTerms() async {
+  return await rootBundle.loadString('assets/terms/privacy_terms.txt');
+}
 
 class Privacy_Terms extends StatefulWidget {
   const Privacy_Terms({super.key});
@@ -14,33 +19,23 @@ class _Privacy_TermsState extends State<Privacy_Terms> {
       appBar: AppBar(
         title: Text('개인 정보 처리 방침'),
       ),
-      body: const SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Text(
-          '1. 개인정보의 처리 목적\n'
-          "<BMT>('Best Move for Transfortation') 는 다음의 목적을 위하여 개인정보를 처리하고 있으며, 다음의 목적 이외의 용도로는 이용하지 않습니다.\n"
-          "고객 가입의사 확인, 고객에 대한 서비스 제공에 따른 본인 식별.인증, 회원자격 유지.관리\n"
-          "2. 개인정보의 처리 및 보유 기간\n"
-          "① 'BMT'는 정보주체로부터 개인정보를 수집할 때 동의 받은 개인정보 보유․이용기간 또는 법령에 따른 개인정보 보유․이용기간 내에서 개인정보를 처리․보유합니다.\n"
-          "② 구체적인 개인정보 처리 및 보유 기간은 다음과 같습니다.\n"
-          "- 고객 가입 및 관리 : 이메일을 사용한 회원가입 및 관리\n"
-          "- 보유 기간 : BMT 탈퇴 시, 즉시 삭제\n"
-          "3. 정보주체와 법정대리인의 권리·의무 및 그 행사방법 이용자는 개인정보주체로써 다음과 같은 권리를 행사할 수 있습니다.\n"
-          "① 정보주체는 'BMT' 에 대해 언제든지 다음 각 호의 개인정보 보호 관련 권리를 행사할 수 있습니다.\n"
-          "1. 개인정보 열람요구\n"
-          "2. 오류 등이 있을 경우 정정 요구\n"
-          "3. 삭제요구\n"
-          "4. 처리정지 요구\n"
-          "4. 처리하는 개인정보의 항목 작성\n"
-          "① 'BTM'는 다음의 개인정보 항목을 처리하고 있습니다.\n"
-          "<'BTM'에서 수집하는 개인정보 항목>\n"
-          "'BTM' 회원 가입 시, 제공 동의를 해주시는 개인정보 수집 항목입니다.\n"
-          "■ 회원 가입 시\n"
-          " - 필수항목 : 이름, 이메일, 전화번호, 연령대\n"
-          " - 수집목적 : BMT 회원관리 및 마케팅 이용\n"
-          " - 보유기간 : 회원 탈퇴 또는 동의철회 시 지체없이 파기\n",
-          style: TextStyle(fontSize: 16.0),
-        ),
+      body: FutureBuilder<String>(
+        future: loadTerms(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // 데이터가 로드되면 표시
+            return SingleChildScrollView(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                snapshot.data ?? '내용을 불러오는 데 실패했습니다.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+            );
+          } else {
+            // 데이터 로딩 중에는 로딩 인디케이터 표시
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
