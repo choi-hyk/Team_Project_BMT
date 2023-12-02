7a6bbc0e3a990a385d11407da00dce522f3d3fa2
CHOI HYUK <8167chhk@naver.com>
2023-12-02 23:33:41 +0900

    뒤로가기 수정

--- lib/menu_widgets/store_page.dart
+++ lib/menu_widgets/store_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:test1/interface.dart';
+import 'package:test1/main.dart';
 // import 'package:provider/provider.dart';
 import 'package:test1/provider_code/user_provider.dart';
 import 'package:test1/menu_widgets/buy_page.dart';
@@ -59,192 +60,209 @@ class _StorePageState extends State<StorePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back_ios_new,
-            color: Colors.black,
-          ), // 뒤로 가기 아이콘
-          onPressed: () {
-            _isSearching = false;
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const InterFace(),
-              ),
-            );
-          },
-        ),
-        title: _isSearching
-            ? TextField(
-                // 검색 모드에서는 검색 창을 표시
-                style: const TextStyle(color: Colors.black),
-                decoration: const InputDecoration(
-                  hintText: '검색어를 입력하세요',
-                  hintStyle: TextStyle(color: Colors.black),
-                ),
-                onChanged: (searchQuery) {
-                  // 검색어 입력 시 동작할 작업 추가
-                },
-              )
-            : const Text(
-                '스토어',
-                style:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-              ),
-        backgroundColor: Theme.of(context).primaryColor,
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: Icon(
-              _isSearching ? Icons.cancel : Icons.search,
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: () async {
+        Navigator.pop(context);
+        currentUI = 'home';
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const InterFace()),
+        );
+
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
               color: Colors.black,
-            ),
+            ), // 뒤로 가기 아이콘
             onPressed: () {
-              setState(() {
-                _isSearching = !_isSearching; // 검색 버튼을 누를 때 검색 모드 토글
-              });
+              Navigator.pop(context);
+              currentUI = "home";
+              _isSearching = false;
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const InterFace(),
+                ),
+              );
             },
           ),
-        ],
-      ),
-      body: Container(
-        color: Theme.of(context).canvasColor,
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-              child: Row(
-                children: [
-                  const SizedBox(
-                    width: 6.5,
+          title: _isSearching
+              ? TextField(
+                  // 검색 모드에서는 검색 창을 표시
+                  style: const TextStyle(color: Colors.black),
+                  decoration: const InputDecoration(
+                    hintText: '검색어를 입력하세요',
+                    hintStyle: TextStyle(color: Colors.black),
                   ),
-                  buildCategoryContainer("편의점", showCategoryList),
-                  const SizedBox(
-                    width: 6.5,
-                  ),
-                  buildCategoryContainer("카페", showCategoryList),
-                  const SizedBox(
-                    width: 6.5,
-                  ),
-                  buildCategoryContainer("상품권", showCategoryList),
-                  const SizedBox(
-                    width: 80.0,
-                  ),
-                  // 리워드 포인트
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50.0),
-                        color: Colors.deepPurple[200],
-                      ),
-                      width: 60,
-                      height: 35,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          "${userProvider.point}p",
-                          style: const TextStyle(
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                  onChanged: (searchQuery) {
+                    // 검색어 입력 시 동작할 작업 추가
+                  },
+                )
+              : const Text(
+                  '스토어',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+          backgroundColor: Theme.of(context).primaryColor,
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(
+                _isSearching ? Icons.cancel : Icons.search,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                setState(() {
+                  _isSearching = !_isSearching; // 검색 버튼을 누를 때 검색 모드 토글
+                });
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          color: Theme.of(context).canvasColor,
+          child: Column(
+            children: [
+              Container(
+                decoration:
+                    BoxDecoration(color: Theme.of(context).primaryColor),
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 6.5,
+                    ),
+                    buildCategoryContainer("편의점", showCategoryList),
+                    const SizedBox(
+                      width: 6.5,
+                    ),
+                    buildCategoryContainer("카페", showCategoryList),
+                    const SizedBox(
+                      width: 6.5,
+                    ),
+                    buildCategoryContainer("상품권", showCategoryList),
+                    const SizedBox(
+                      width: 80.0,
+                    ),
+                    // 리워드 포인트
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50.0),
+                          color: Colors.deepPurple[200],
+                        ),
+                        width: 60,
+                        height: 35,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            "${userProvider.point}p",
+                            style: const TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 6.5,
-            ),
-            Expanded(
-              child: StreamBuilder(
-                stream:
-                    FirebaseFirestore.instance.collection('Store').snapshots(),
-                builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return const CircularProgressIndicator();
-                  }
+              const SizedBox(
+                height: 6.5,
+              ),
+              Expanded(
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance
+                      .collection('Store')
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const CircularProgressIndicator();
+                    }
 
-                  var stores = snapshot.data!.docs;
+                    var stores = snapshot.data!.docs;
 
-                  return ListView.builder(
-                    itemCount: stores.length,
-                    itemBuilder: (context, index) {
-                      var store = stores[index].data();
+                    return ListView.builder(
+                      itemCount: stores.length,
+                      itemBuilder: (context, index) {
+                        var store = stores[index].data();
 
-                      return Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                // 이미지를 탭하면 구매 페이지로 이동
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => BuyPage(
-                                      category: store['category'],
-                                      categoryname: store['category_name'],
-                                      icon: store['icon'],
-                                      imageUrl: store['image_url'],
-                                      name: store['name'],
-                                      pay: store['pay'],
-                                      giftUrl: store['gift_url'],
+                        return Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  // 이미지를 탭하면 구매 페이지로 이동
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => BuyPage(
+                                        category: store['category'],
+                                        categoryname: store['category_name'],
+                                        icon: store['icon'],
+                                        imageUrl: store['image_url'],
+                                        name: store['name'],
+                                        pay: store['pay'],
+                                        giftUrl: store['gift_url'],
+                                      ),
                                     ),
-                                  ),
-                                );
-                              },
-                              child: Container(
-                                width: double.infinity,
-                                height: 110,
-                                decoration: const BoxDecoration(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20)),
-                                    color: Colors.white),
-                                child: ListTile(
-                                  title: Text(
-                                    store['name'],
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Theme.of(context).primaryColorDark),
-                                  ),
-                                  subtitle: Text(
-                                    ' ${store['pay']}p',
-                                    textAlign: TextAlign.start,
-                                    style: TextStyle(
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.bold,
-                                        color:
-                                            Theme.of(context).primaryColorDark),
-                                  ),
-                                  leading: SizedBox(
-                                    width: 90,
-                                    height: 130,
-                                    child: Image.network(
-                                      store['image_url'],
-                                      fit: BoxFit.fill,
+                                  );
+                                },
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 110,
+                                  decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      color: Colors.white),
+                                  child: ListTile(
+                                    title: Text(
+                                      store['name'],
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Theme.of(context)
+                                              .primaryColorDark),
+                                    ),
+                                    subtitle: Text(
+                                      ' ${store['pay']}p',
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                          fontSize: 13,
+                                          fontWeight: FontWeight.bold,
+                                          color: Theme.of(context)
+                                              .primaryColorDark),
+                                    ),
+                                    leading: SizedBox(
+                                      width: 90,
+                                      height: 130,
+                                      child: Image.network(
+                                        store['image_url'],
+                                        fit: BoxFit.fill,
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                },
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
